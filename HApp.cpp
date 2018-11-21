66649712284aebce38ea8139368aabe5f34aa8b8
Julian Hall <jajhall@ed.ac.uk>
2018-11-21 17:14:38 +0000

    Adding sensitivity and ranging error measure

--- HApp.cpp
+++ HApp.cpp
@@ -10,9 +10,17 @@ extern HModel hmodel;
 
 #include "hems.h"
 
+void checkSolve(HModel *model) {
+  model->setup();
+  //  model->scale();
+  model->solve();
+}
+
 void test_mps(char *filename) {
+        const double HEMS_INF = 1e31;
 	// Input all data in MPS format
 	HInput mps_input;
+
 	mps_input.loadMps(filename);
 	int numCol = mps_input.numCol;
 	int numRow = mps_input.numRow;
@@ -35,7 +43,7 @@ void test_mps(char *filename) {
 	hems_lmdl(&returnCode, 0, 2, numRow, numCol, AcountX, &cost[0],
 			&rowLower[0], &rowUpper[0], &colLower[0], &colUpper[0], &Aindex[0],
 			&Astart[0], &Avalue[0]);
-	hems_scal(&returnCode, 0);
+	//	hems_scal(&returnCode, 0);
 	hems_sslv(&returnCode, 0, 0, 0);
 
 	for (int i = 0; i <= numCol; i++)
@@ -75,6 +83,12 @@ void test_mps(char *filename) {
 			&rb_up_b[0], &rb_dn_b[0], &rb_up_f[0], &rb_dn_f[0], &rb_up_e[0],
 			&rb_dn_e[0], &rb_up_l[0], &rb_dn_l[0]);
 
+	double totalSensitivityDataError = 0;
+	const double toleranceRelativeTotalError = 1e-15;
+	const double toleranceRelativeError = toleranceRelativeTotalError;
+	const double relativeErrorDenominator = max(1.0, abs(hmodel.objective));
+	bool reportSensitivityDataCheck = numCol+numRow < 100;
+
 	vector<int> Nflag = hmodel.houtput.Nflag;
 	vector<int> Nmove = hmodel.houtput.Nmove;
 
@@ -87,48 +101,66 @@ void test_mps(char *filename) {
 		double solved_up = 0;
 		double solved_dn = 0;
 		{
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (Nflag[i + numCol]) {
-				if (Nmove[i + numCol] == 0) {
-					tmodel.hinput.rowLower[i] = rb_dn_b[i];
-					tmodel.hinput.rowUpper[i] = rb_dn_b[i];
-				} else if (Nmove[i + numCol] == -1) {
-					tmodel.hinput.rowLower[i] = rb_dn_b[i];
-				} else {
-					tmodel.hinput.rowUpper[i] = rb_dn_b[i];
-				}
+			if (rb_dn_b[i] > -HEMS_INF) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  if (Nflag[i + numCol]) {
+			    if (Nmove[i + numCol] == 0) {
+			      tmodel.hinput.rowLower[i] = rb_dn_b[i];
+			      tmodel.hinput.rowUpper[i] = rb_dn_b[i];
+			    } else if (Nmove[i + numCol] == -1) {
+			      tmodel.hinput.rowLower[i] = rb_dn_b[i];
+			    } else {
+			      tmodel.hinput.rowUpper[i] = rb_dn_b[i];
+			    }
+			  } else {
+			    tmodel.hinput.rowUpper[i] = rb_dn_b[i];
+			  }
+			  checkSolve(&tmodel);
+			  solved_dn = tmodel.objective;
 			} else {
-				tmodel.hinput.rowUpper[i] = rb_dn_b[i];
+			  solved_dn = rb_dn_f[i];
 			}
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_dn = tmodel.objective;
+			double error = abs(solved_dn-rb_dn_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Row bound down ranging error: Row %2d; solved_dn = %12g; b_dn_f = %12g; relativeError = %12g\n",
+				 i, solved_dn, rb_dn_f[i], relativeError);
+			}
 		}
 
 		{
 
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (Nflag[i + numCol]) {
-				if (Nmove[i + numCol] == 0) {
-					tmodel.hinput.rowLower[i] = rb_up_b[i];
-					tmodel.hinput.rowUpper[i] = rb_up_b[i];
-				} else if (Nmove[i + numCol] == -1) {
-					tmodel.hinput.rowLower[i] = rb_up_b[i];
-				} else {
-					tmodel.hinput.rowUpper[i] = rb_up_b[i];
-				}
+			if (rb_up_b[i] < HEMS_INF) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  if (Nflag[i + numCol]) {
+			    if (Nmove[i + numCol] == 0) {
+			      tmodel.hinput.rowLower[i] = rb_up_b[i];
+			      tmodel.hinput.rowUpper[i] = rb_up_b[i];
+			    } else if (Nmove[i + numCol] == -1) {
+			      tmodel.hinput.rowLower[i] = rb_up_b[i];
+			    } else {
+			      tmodel.hinput.rowUpper[i] = rb_up_b[i];
+			    }
+			  } else {
+			    tmodel.hinput.rowLower[i] = rb_up_b[i];
+			  }
+			  checkSolve(&tmodel);
+			  solved_up = tmodel.objective;
 			} else {
-				tmodel.hinput.rowLower[i] = rb_up_b[i];
+			  solved_up = rb_up_f[i];
 			}
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_up = tmodel.objective;
+			double error = abs(solved_up-rb_up_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Row bound   up ranging error: Row %2d; solved_up = %12g; b_up_f = %12g; relativeError = %12g\n",
+				 i, solved_up, rb_up_f[i], relativeError);
+			}
 		}
 		printf("%3d %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g\n",
 				i, rowLower[i], rowUpper[i], rowValue[i], 0.0, rowDual[i],
@@ -145,48 +177,67 @@ void test_mps(char *filename) {
 		double solved_up = 0;
 		double solved_dn = 0;
 		{
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (Nflag[i]) {
-				if (Nmove[i] == 0) {
-					tmodel.hinput.colLower[i] = cb_dn_b[i];
-					tmodel.hinput.colUpper[i] = cb_dn_b[i];
-				} else if (Nmove[i] == 1) {
-					tmodel.hinput.colLower[i] = cb_dn_b[i];
-				} else {
-					tmodel.hinput.colUpper[i] = cb_dn_b[i];
-				}
+    		        if (cb_dn_b[i] > -HEMS_INF) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  if (Nflag[i]) {
+			    if (Nmove[i] == 0) {
+			      tmodel.hinput.colLower[i] = cb_dn_b[i];
+			      tmodel.hinput.colUpper[i] = cb_dn_b[i];
+			    } else if (Nmove[i] == 1) {
+			      tmodel.hinput.colLower[i] = cb_dn_b[i];
+			    } else {
+			      tmodel.hinput.colUpper[i] = cb_dn_b[i];
+			    }
+			  } else {
+			    tmodel.hinput.colUpper[i] = cb_dn_b[i];
+			  }
+			  checkSolve(&tmodel);
+			  solved_dn = tmodel.objective;
 			} else {
-				tmodel.hinput.colUpper[i] = cb_dn_b[i];
+			  solved_dn = cb_dn_f[i];
 			}
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_dn = tmodel.objective;
+			double error = abs(solved_dn-cb_dn_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Col bound down ranging error: Col %2d; solved_dn = %12g; b_dn_f = %12g; relativeError = %12g\n",
+				 i, solved_dn, cb_dn_f[i], relativeError);
+			}
+//#endif
 		}
 
 		{
 
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (Nflag[i]) {
-				if (Nmove[i] == 0) {
-					tmodel.hinput.colLower[i] = cb_up_b[i];
-					tmodel.hinput.colUpper[i] = cb_up_b[i];
-				} else if (Nmove[i] == 1) {
-					tmodel.hinput.colLower[i] = cb_up_b[i];
-				} else {
-					tmodel.hinput.colUpper[i] = cb_up_b[i];
-				}
+ 		        if (cb_up_b[i] < HEMS_INF) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  if (Nflag[i]) {
+			    if (Nmove[i] == 0) {
+			      tmodel.hinput.colLower[i] = cb_up_b[i];
+			      tmodel.hinput.colUpper[i] = cb_up_b[i];
+			    } else if (Nmove[i] == 1) {
+			      tmodel.hinput.colLower[i] = cb_up_b[i];
+			    } else {
+			      tmodel.hinput.colUpper[i] = cb_up_b[i];
+			    }
+			  } else {
+			    tmodel.hinput.colLower[i] = cb_up_b[i];
+			  }
+			  checkSolve(&tmodel);
+			  solved_up = tmodel.objective;
 			} else {
-				tmodel.hinput.colLower[i] = cb_up_b[i];
+			  solved_up = cb_up_f[i];
 			}
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_up = tmodel.objective;
+			double error = abs(solved_up-cb_up_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Col bound   up ranging error: Col %2d; solved_up = %12g; b_up_f = %12g; relativeError = %12g\n",
+				 i, solved_up, cb_up_f[i], relativeError);
+			}
 		}
 		printf("%3d %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g\n",
 				i, colLower[i], colUpper[i], colValue[i], cost[i], colDual[i],
@@ -204,28 +255,44 @@ void test_mps(char *filename) {
 		double solved_up = 0;
 		double solved_dn = 0;
 		{
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (fabs(co_dn_c[i]) < 1e30)
-				tmodel.hinput.cost[i] = co_dn_c[i];
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_dn = tmodel.objective;
+		        if (fabs(co_dn_c[i]) < 1e30) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  tmodel.hinput.cost[i] = co_dn_c[i];
+			  checkSolve(&tmodel);
+			  solved_dn = tmodel.objective;
+			} else {
+			  solved_dn = co_dn_f[i];
+			}
+			double error = abs(solved_dn-co_dn_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Col  cost down ranging error: Col %2d; solved_dn = %12g; c_dn_f = %12g; relativeError = %12g\n",
+				 i, solved_dn, co_dn_f[i], relativeError);
+			}
 		}
 
 		{
 
-			HModel tmodel;
-			tmodel.solving_with_log = false;
-			tmodel.hinput = mps_input;
-			if (fabs(co_up_c[i]) < 1e30)
-				tmodel.hinput.cost[i] = co_up_c[i];
-			tmodel.setup();
-			tmodel.scale();
-			tmodel.solve();
-			solved_up = tmodel.objective;
+		        if (fabs(co_up_c[i]) < 1e30) {
+			  HModel tmodel;
+			  tmodel.solving_with_log = false;
+			  tmodel.hinput = mps_input;
+			  tmodel.hinput.cost[i] = co_up_c[i];
+			  checkSolve(&tmodel);
+			  solved_up = tmodel.objective;
+			} else {
+			  solved_up = co_up_f[i];
+			}
+			double error = abs(solved_up-co_up_f[i]);
+			totalSensitivityDataError += error;
+			double relativeError = error/relativeErrorDenominator;
+			if (relativeError > toleranceRelativeError) {
+			  printf("Col  cost   up ranging error: Col %2d; solved_up = %12g; c_up_f = %12g; relativeError = %12g\n",
+				 i, solved_up, co_up_f[i], relativeError);
+			}
 		}
 		printf("%3d %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g %12g\n",
 				i, colLower[i], colUpper[i], colValue[i], cost[i], colDual[i],
@@ -233,6 +300,10 @@ void test_mps(char *filename) {
 				solved_dn);
 	}
 	printf("\n\n");
+	totalSensitivityDataError /= relativeErrorDenominator;
+	if (totalSensitivityDataError > toleranceRelativeTotalError) {
+	  printf("WARNING totalSensitivityDataError = %12g\n", totalSensitivityDataError);
+	}
 
 	cout << endl;
 }
